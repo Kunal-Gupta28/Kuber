0c6a32b9cde348be6ba2dc2b33ee78df7e3b5085
Kunal-Gupta28 <kunal.gmail.91165@gmail.com>
2025-05-11 13:26:09 +0530

    finding cors error

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -43,8 +43,12 @@ const UserLogin = () => {
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_BASE_URL}/users/login`,
-        userData
+        userData,
+        {
+          withCredentials: true
+        }
       );
+      
 
       if (response.status === 200) {
         setUser(response.data.user);
