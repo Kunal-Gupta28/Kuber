3c8515c4ccd8109ef2f2453691e1fe49c3cfc03a
Kunal Gupta <150496076+Kunal-Gupta28@users.noreply.github.com>
2025-05-23 11:14:05 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
-import CaptainContextProvider from "./context/CaptainContext.jsx";  // Correct default import
-import SocketProvider from "./context/socketContext.jsx";
+import CaptainContextProvider from "./context/CaptainContext.jsx";
+import {SocketProvider} from "./context/socketContext.jsx";
 import { RideContextProvider } from "./context/RideContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CaptainContextProvider> {/* Correct context provider usage */}
+    <CaptainContextProvider>
       <UserContext>
         <SocketProvider>
           <RideContextProvider>
